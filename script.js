36054794f7ac3d5817f5975b1e6615f3732cabe2
Marie Miatova <mashamaratova@gmail.com>
2023-11-08 14:38:43 +0300

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
-}
+  return Array(arraySize).fill(value);
+};
 
-const data = 3;
+const arraySize = 3;
 const valueToFill = 'a';
-console.log(fill(data, valueToFill))
+console.log(fill(arraySize, valueToFill));
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
- }
+  return array.reverse();
+};
 
-const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+const dataArray = [1, 2, 3];
+console.log(reverse(dataArray)); // [3, 2, 1]
 
 // задача №3
 const compact = (array) => {
-  // решение здесь
-}
+  const result = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] !== 0 && array[i] !== false && array[i] !== undefined && array[i] !== '' && array[i] !== null) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+};
 
 const data = [0, 1, false, 2, undefined, '', 3, null];
-console.log(compact(data)) // [1, 2, 3]
+console.log(compact(data)); // [1, 2, 3]
